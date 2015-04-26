8e2a0f93f7077499d202a06970d7ad6fab370de5
Azure22 <hanx.azure22@gmail.com>
2015-04-25 20:04:09 -0700

    Reorganized testcases, renamed loadFile to textFile

--- RDD.py
+++ RDD.py
@@ -212,13 +212,13 @@ class ReduceByKey(RDD):
 '''
 class Spark(object):
 
-    def loadData(self, data):
-        return LoadData(data)
+    def textData(self, data):
+        return TextData(data)
 
-    def loadFile(self, path):
-        return LoadFile(path)
+    def textFile(self, path):
+        return TextFile(path)
 
-class LoadData(RDD):
+class TextData(RDD):
 
     def __init__(self, data):
         RDD.__init__(self)
@@ -229,7 +229,7 @@ class LoadData(RDD):
         for r in iter(self.data):
             yield r
 
-class LoadFile(RDD):
+class TextFile(RDD):
 
     def __init__(self, path):
         RDD.__init__(self)
@@ -243,47 +243,29 @@ class LoadFile(RDD):
         for r in iter(self.elements):
             yield r
 
-def computeContribs(urls, rank):
-    """Calculates URL contributions to the rank of other URLs."""
-    num_urls = len(urls)
-    for url in urls:
-        yield (url, rank / num_urls)
-
-def parseNeighbors(urls):
-    """Parses a urls pair string into urls pair."""
-    parts = re.split(r'\s+', urls)
-    return parts[0], parts[1]
-
-if __name__ == '__main__':
-    spark = Spark()
-    data = [1,2,3,4,5]
-    RDDA = spark.loadData(data).map(lambda x: x + 1).filter(lambda x: x > 3)
-    RDDB = spark.loadData(data).map(lambda x: x + 4).filter(lambda x: x > 8)
-    #print RDDA.collect()
-    #print RDDB.collect()
-    RDDAB = RDDA.union(RDDB)
-    #print RDDAB.collect()
-    #print RDDAB.reduce(lambda a, b: a + b)
-    #print RDDAB.flatMap(lambda x: range(x)).collect()
-    #print RDDA.crossProduct(RDDB).collect()
-    
-    data2 = [('a', 1), ('b', 2), ('c', 3), ('d', 5), ('a', 6), ('d', 12)]
-    data3 = [('a', 6), ('b', 7), ('c', 10), ('d', 2)]
-    RDDC = spark.loadData(data2)
-    RDDD = spark.loadData(data3)
-    #print RDDC.collect()
-    #print RDDD.collect()
-    #print RDDC.join(RDDD).collect()
-    #print RDDC.groupByKey().collect()
-    #print RDDC.reduceByKey(lambda a, b: a + b).collect()
-    lines = spark.loadFile("wordcount.txt")
+def doWordCount(path):
+    print "Start word counting"
+    lines = spark.textFile(path)
     words = lines.flatMap(lambda line: line.split(" "))
     wordDict = words.map(lambda word: (word, 1))
     counts = wordDict.reduceByKey(lambda a, b: a + b)
-    #print counts.result
-    #print counts.collect()
+    print counts.collect()
 
-    lines = spark.loadFile("pagerank.txt")
+def doPageRank(path):
+
+    def computeContribs(urls, rank):
+        """Calculates URL contributions to the rank of other URLs."""
+        num_urls = len(urls)
+        for url in urls:
+            yield (url, rank / num_urls)
+
+    def parseNeighbors(urls):
+        """Parses a urls pair string into urls pair."""
+        parts = re.split(r'\s+', urls)
+        return parts[0], parts[1]
+
+    print "Start page ranking"
+    lines = spark.textFile(path)
     links = lines.map(lambda urls: parseNeighbors(urls)).groupByKey()
     ranks = links.map(lambda url_neighbors: (url_neighbors[0], 1.0))
     # Calculates and updates URL ranks continuously using PageRank algorithm.
@@ -297,3 +279,29 @@ if __name__ == '__main__':
     # Collects all URL ranks and dump them to console.
     for (link, rank) in ranks.collect():
         print("%s has rank: %s." % (link, rank))
+
+if __name__ == '__main__':
+    spark = Spark()
+    data = [1,2,3,4,5]
+    RDDA = spark.textData(data).map(lambda x: x + 1).filter(lambda x: x > 3)
+    RDDB = spark.textData(data).map(lambda x: x + 4).filter(lambda x: x > 8)
+    #print RDDA.collect()
+    #print RDDB.collect()
+    RDDAB = RDDA.union(RDDB)
+    #print RDDAB.collect()
+    #print RDDAB.reduce(lambda a, b: a + b)
+    #print RDDAB.flatMap(lambda x: range(x)).collect()
+    #print RDDA.crossProduct(RDDB).collect()
+    
+    data2 = [('a', 1), ('b', 2), ('c', 3), ('d', 5), ('a', 6), ('d', 12)]
+    data3 = [('a', 6), ('b', 7), ('c', 10), ('d', 2)]
+    RDDC = spark.textData(data2)
+    RDDD = spark.textData(data3)
+    #print RDDC.collect()
+    #print RDDD.collect()
+    #print RDDC.join(RDDD).collect()
+    #print RDDC.groupByKey().collect()
+    #print RDDC.reduceByKey(lambda a, b: a + b).collect()
+
+    doWordCount("wordcount.txt")
+    doPageRank("pagerank.txt")
