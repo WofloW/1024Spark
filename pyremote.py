11d946fdf8395033c8a01ed4c33670ad7240d3c7
WofloW <lancefangw@gmail.com>
2015-05-07 11:54:18 -0700

    split file(support multiple file), remote start worker

---
+++ pyremote.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+#python pyremote.py localhost start_worker.sh
+
+import os
+import os.path
+import subprocess
+import sys
+
+class Remote(object):
+    
+    def __init__(self, host, command):
+        self.host    = host
+        self.command = command
+        self.curdir  = os.getcwd()
+        self.remote_command = os.path.join(self.curdir, self.command)
+
+
+    def run(self):
+        proc = subprocess.call(['ssh', self.host, self.remote_command])
+
+if __name__ == '__main__':
+    remote = Remote(sys.argv[1], sys.argv[2])
+    remote.run()
---
+++ start_worker.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python /Users/WofloW/USF/CS636/1024Spark/split_file.py
